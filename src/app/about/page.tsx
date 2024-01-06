2ee89959082df2b8845c4f46b5cffcaa8f82f657
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2024-01-06 13:21:25 -0700

    feat: 🎸 about
    added under construction message

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,18 @@
-import { Text } from "@mantine/core";
+import { Center, Group, Stack, Text, Title } from "@mantine/core";
+import { IconBackhoe } from "@tabler/icons-react";
 
 const AboutPage = () => {
-    return <Text></Text>;
+    return (
+        <Center h={"100%"}>
+            <Group gap={"lg"}>
+                <IconBackhoe size={"6rem"} />
+                <Stack gap={0}>
+                    <Title>Under construction</Title>
+                    <Text>Please check back later!</Text>
+                </Stack>
+            </Group>
+        </Center>
+    );
 };
 
 export default AboutPage;
